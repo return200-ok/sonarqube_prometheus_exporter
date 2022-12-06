cd56bf67a8c0a5f1fa8c99b61b5e8deb87a787c6
caolv <caolv@biplus.com.vn>
2022-12-06 08:37:54 +0700

    .

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM 3.8-alpine
+FROM python:3.8-alpine
 
 WORKDIR /root/
 COPY . .
 RUN pip3 install -r requirements.txt
 
 EXPOSE 8098
-ENTRYPOINT [ "/bin/bash",  "entrypoint.sh" ]
+ENTRYPOINT [ "/bin/sh",  "entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,2 @@
-#!/usr/bin/env bash
-
-if [ $SONAR_SERVER ]; then
-    sed -i "s~http://sonar_server~$SONAR_SERVER~g" config.ini
-fi
-
-if [ $SONAR_USERNAME ]; then
-    sed -i "s/username/$SONAR_USERNAME/g" config.ini
-fi
-
-if [ $SONAR_PASSWORD ]; then
-    sed -i "s/password/$SONAR_PASSWORD/g" config.ini
-fi
-
-# Run exporter
-/usr/local/bin/python main.py
+#!/bin/sh
+/usr/local/bin/python3 main.py
