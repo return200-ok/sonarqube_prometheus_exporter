a2ce83ae993dbacad25f64911f19f7b000e48c8d
caolv <caolv@biplus.com.vn>
2022-12-14 14:49:51 +0700

    init utils

--- lib/until.py
+++ lib/until.py
@@ -1,9 +1,11 @@
 import json
 import logging
+import re
 import sys
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pandas as pd
+import requests
 import rfc3339
 
 '''
@@ -38,3 +40,43 @@ def sr_to_json(series):
     d = fre.to_json()
     j_data = json.loads(d)
     return j_data
+
+def get_json(element, json_data):
+  if element in json_data:
+    return json_data[element]
+  else:
+    return 0
+
+#Get data from url and convert to JSON
+def get_data(url, token):
+  session = requests.Session()
+  session.auth = token, ''
+  call = getattr(session, 'get')
+  res = call(url)
+  data = json.loads(res.content)
+  return data
+
+def convert(string):
+    value = re.search(r'([0-9]+)', string)
+    unit = re.search(r'([A-Z]?B)', string)
+
+    num = int(value.group())
+    unit = unit.group()
+    if unit == 'B':
+        return num
+    num *= 1024
+
+    if unit == 'KB':
+        return num
+    num *= 1024
+
+    if unit == 'MB':
+        return num
+    num *= 1024
+
+    if unit == 'GB':
+        return num
+    num *= 1024
+
+    if unit == 'TB':
+        return num
